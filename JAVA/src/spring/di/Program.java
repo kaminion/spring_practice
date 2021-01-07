1aa03300a33a56799384915ced3af06a98aa0250
Declan Andrew <anysweetimes@naver.com>
2021-01-07 23:24:16 +0900

    beans 세팅 및 maven 프로젝트로 전환

--- JAVA/src/spring/di/Program.java
+++ JAVA/src/spring/di/Program.java
@@ -1,5 +1,6 @@
 package spring.di;
 
+
 import spring.di.entity.Exam;
 import spring.di.entity.NewlectExam;
 import spring.di.ui.ExamConsole;
@@ -9,11 +10,24 @@ import spring.di.ui.InlineExamConsole;
 public class Program {
 
 	public static void main(String[] args) {
-		Exam exam = new NewlectExam();
-		//ExamConsole console = new InlineExamConsole(exam);
-		ExamConsole console = new GridExamConsole(exam);
+		/*Exam exam = new NewlectExam();
+		//ExamConsole console = new InlineExamConsole(exam); // DI
+		ExamConsole console = new GridExamConsole();
+		console.setExam(exam);*/
+		
+		// ApplicationContext 종류
+		// classPathXML = 어플리케이션의 루트로부터
+		// FileSystemXML = 현재 파일시스템의 루트로부터 시작
+		// XMLWeb = 웹을 통해 지정
+		// AnnotationConfig 파일이 아닌 어노테이션으로 두었다
+		
+		ApplicationContext context =
+				new ClassPathXmlApplicationContext("spring/di/setting.xml");
+		
+		// 스프링에게 지시하는 코드로 변경
+		ExamConsole console = ?
 		console.print();
-
+		
 	}
 
 }
--- JAVA/src/spring/di/ui/GridExamConsole.java
+++ JAVA/src/spring/di/ui/GridExamConsole.java
@@ -6,6 +6,10 @@ public class GridExamConsole implements ExamConsole {
 
 	private Exam exam;
 	
+	public GridExamConsole() {
+		// TODO Auto-generated constructor stub
+	}
+	
 	public GridExamConsole(Exam exam) {
 		super();
 		this.exam = exam;
@@ -16,6 +20,12 @@ public class GridExamConsole implements ExamConsole {
 		System.out.println("===================================================");
 		System.out.printf("total %d, avg %f\n", exam.total(), exam.avg());
 		System.out.println("===================================================");
+	}
+
+	@Override
+	public void setExam(Exam exam) {
+		// TODO Auto-generated method stub
+		this.exam = exam;
 	}
 
 }
